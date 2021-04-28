6517fc871481bc5bbc6d0e1370410fec5db3a258
Truong Hong Thai <truonghongthai97@gmail.com>
2021-04-28 22:27:19 +0700

    add text  2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.aaaaa
+          Edit <code>src/App.js</code> and save to reload.aaaaaaaa
         </p>
         <a
           className="App-link"
